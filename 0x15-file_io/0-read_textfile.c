60e907c0943433a4f88e9a1bafd1e9d302825fe2
Ben Wasonga <65865227+bmwasonga@users.noreply.github.com>
2022-06-03 23:14:40 +0300

    donkey

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,48 +1,36 @@
 #include "main.h"
 
-/*Library for open system call man open*/
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-
-/*Library for close and write system call man close*/
-#include <unistd.h>
-
 /**
- * read_textfile - Function that reads a text file and prints it
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
  *
- * @filename: The pathname of file to read
- * @letters: number of letters it should read and print
- *
- * Description: A function that reads a text file and prints it to
- * the POSIX standard output.
- * Return: Number of letters it could read and print, otherwise 0
+ * Return: numbers of letters printed. It fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
-	char *buffer;
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
 		return (0);
 
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
-
-	if (o == -1 || r == -1 || w == -1 || w != r)
-	{
-		free(buffer);
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
 		return (0);
-	}
 
-	free(buffer);
-	close(o);
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
-	return (w);
+	close(fd);
+
+	free(buf);
+
+	return (nwr);
 }
-
